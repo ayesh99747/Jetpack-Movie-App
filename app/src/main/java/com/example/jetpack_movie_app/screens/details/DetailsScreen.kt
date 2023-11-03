7b8c9b56c8e3b9bd125c42a850539ac29cb54c05
Ayeshmantha Madara Wijayagunethilake <ayesh99747@gmail.com>
2023-11-03 16:05:47 +0000

    Added back button on the TopBar.

--- app/src/main/java/com/example/jetpack_movie_app/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/jetpack_movie_app/screens/details/DetailsScreen.kt
@@ -1,42 +1,71 @@
 package com.example.jetpack_movie_app.screens.details
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(navController: NavController, movieData: String?){
+fun DetailsScreen(navController: NavController, movieData: String?) {
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            title = { Text(text = "Movies", style = MaterialTheme.typography.titleLarge) },
+            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Blue),
+            navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Localized description"
+                    )
+                }
+            }
+        )
+    })
+    {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = movieData.toString(), style = MaterialTheme.typography.headlineLarge)
+                Spacer(modifier = Modifier.height(23.dp))
 
-    
-    Surface (
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ){
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ){
-            Text(text = movieData.toString(), style = MaterialTheme.typography.headlineLarge)
-            Spacer(modifier = Modifier.height(23.dp))
-            Button(onClick = {
-                navController.popBackStack()
-            }) {
-                Text(text = "Go Back")
             }
         }
     }
+
+
 }
--- app/src/main/java/com/example/jetpack_movie_app/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/jetpack_movie_app/screens/home/HomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -39,8 +40,8 @@ fun HomeScreen(navController:NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Movies") },
-                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Magenta)
+                title = { Text(text = "Movies", style = MaterialTheme.typography.titleLarge) },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Blue)
             )
         }
     ) {
