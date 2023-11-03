62b82d73e8ac14a3126a96a7885fc7e67f822b73
Ayeshmantha Madara Wijayagunethilake <ayesh99747@gmail.com>
2023-11-03 12:23:44 +0000

    Added Scaffolding and Movie Rows.

--- app/src/main/java/com/example/jetpack_movie_app/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_movie_app/MainActivity.kt
@@ -1,16 +1,45 @@
 package com.example.jetpack_movie_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MovableContent
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_movie_app.ui.theme.JetpackMovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,26 +52,92 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetpackMovieAppTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Movies") },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Magenta)
+                )
+            }
+        ) {
+
+            content()
+        }
+    }
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Happiness",
+        "Cross The Line",
+        "Be Happy",
+        "Happy Feet",
+        "Life"
     )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it) { movie ->
+
+                }
+            }
+        }
+    }
 }
+
+@Composable
+fun MovieRow(movie: String, onItemClicked: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClicked(movie)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackMovieAppTheme {
-        Greeting("Android")
+fun DefaultPreview() {
+    MyApp {
+        MainContent()
     }
 }
